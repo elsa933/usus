fc2a60a20847815c6295368818c2ef0e42322b3f
elsa933 <89612591+elsa933@users.noreply.github.com>
2021-08-27 05:57:00 +0800

    Create susu.sh

---
+++ susu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get install wget -y
+wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 4
