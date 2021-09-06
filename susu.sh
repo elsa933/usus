4968e8d6e684c4b606fa71c81940f9daefb46776
elsa933 <89612591+elsa933@users.noreply.github.com>
2021-09-05 19:31:51 -0700

    Update susu.sh

--- susu.sh
+++ susu.sh
@@ -2,4 +2,4 @@
 apt-get install wget -y
 wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 4
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.colabs31 -p x --cpu 40
